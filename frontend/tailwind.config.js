1bb3111e3f5abc547d95b5cde142f41676ea5be1
kelvinempire <kelvinewurum@gmail.com>
2025-01-26 19:21:33 +0100

    Update Tailwind CSS configuration to support Vue and add custom gradient background

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	// eslint-disable-next-line no-undef
-	plugins: [require("daisyui")],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(45deg, rgb(169, 50, 210), rgb(110, 50, 207), rgb(57, 50, 187))",
+      },
+    },
+  },
+  plugins: [],
 };
